eedb0783ffd3b26b9e817104aafb849fdfe5d7ab
gnuhua <gnu.hua@gmail.com>
2016-04-03 22:59:34 +0800

    NSApp

---
+++ showShortcuts/showShortcuts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  showShortcuts
+//
+//  Created by huatian on 16/4/3.
+//  Copyright © 2016年 iosdevlog. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
